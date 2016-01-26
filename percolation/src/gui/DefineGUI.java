09b6ec83c18b3de89bd63d1dbb68fadfa875ac31
Marine Doyen-Le Boulaire <marine.doyen-le-boulaire@ensimag.grenoble-inp.fr>
2016-01-26 14:47:16 +0100

    Agencement de la GUI de definition

--- percolation/src/gui/DefineGUI.java
+++ percolation/src/gui/DefineGUI.java
@@ -5,16 +5,16 @@
  */
 package gui;
 
-import java.awt.BorderLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import percolation.Forest;
@@ -24,13 +24,25 @@ import percolation.ForestException;
  *
  * @author doyenm
  */
-public class DefineGUI extends JFrame {
+public final class DefineGUI extends JFrame {
 
     public JTextField widthTF;
     public JTextField heightTF;
     public JTextField pTF;
     public JTextField eTF;
     public JTextField eMajTF;
+
+    public void oneFieldBuilding(GridBagConstraints c, JPanel pane,
+            String label, int gridy, JTextField tf) {
+        JLabel jLabel = new JLabel(label);
+        c.gridx = 0;
+        c.gridy = gridy;
+        pane.add(jLabel, c);
+        widthTF = new JTextField("10", 20);
+        c.gridx = 1;
+        c.gridy = gridy;
+        pane.add(tf, c);
+    }
 
     public DefineGUI() {
         super("Definition of the wood");
@@ -41,24 +53,28 @@ public class DefineGUI extends JFrame {
         };
         addWindowListener(l);
 
-        JButton button = new JButton("Define");
-        button.addActionListener(new CreateListener(this));
+        JPanel pane = new JPanel(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+        c.fill = GridBagConstraints.HORIZONTAL;
 
         widthTF = new JTextField("10", 20);
+        oneFieldBuilding(c, pane, "Width : ", 0, widthTF);
         heightTF = new JTextField("10", 20);
+        oneFieldBuilding(c, pane, "Height : ", 1, heightTF);
         pTF = new JTextField("0.5", 20);
+        oneFieldBuilding(c, pane, "p : ", 2, pTF);
         eTF = new JTextField("0.1", 20);
+        oneFieldBuilding(c, pane, "e : ", 3, eTF);
         eMajTF = new JTextField("0.05", 20);
+        oneFieldBuilding(c, pane, "eMaj : ", 4, eMajTF);
 
-        JPanel panel = new JPanel();
-        panel.add(widthTF, BorderLayout.CENTER);
-        panel.add(heightTF, BorderLayout.CENTER);
-        panel.add(pTF);
-        panel.add(eTF);
-        panel.add(eMajTF);
-        panel.add(button);
+        JButton button = new JButton("Define");
+        button.addActionListener(new CreateListener(this));
+        c.gridx = 0;
+        c.gridy = 5;
+        pane.add(button, c);
 
-        setContentPane(panel);
+        setContentPane(pane);
         setSize(500, 300);
         setVisible(true);
     }
@@ -91,7 +107,7 @@ public class DefineGUI extends JFrame {
                 System.out.println("Situation initiale : ");
                 basicGUI.view(forrest.getForestArray(), width, height);
                 forrest.fire(p);
-            } catch(ForestException fEx){
+            } catch (ForestException fEx) {
                 fEx.printStackTrace();
             }
         }
